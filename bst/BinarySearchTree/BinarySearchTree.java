794cd967949f6428b985e0f4404849ff64433d53
maykonestacio <maykon.estacio29@gmail.com>
2025-01-07 17:34:46 +0000

    SAVEPOINT

---
+++ bst/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,63 @@
+package bst.BinarySearchTree;
+
+import bst.Node.*;
+
+public class BinarySearchTree { 
+
+    Node root;
+
+    public void insert(Node node) { 
+
+        this.root = this.insertHelper(this.root, node);
+
+    }
+    private Node insertHelper(Node root, Node node) {
+
+        int data = node.data;
+
+        if (root == null) {
+            
+            root = node;
+
+            return root;
+
+        }else if(data < root.data){
+
+            root.left = insertHelper(root.left, node);
+
+        }
+        else {
+
+            root.right = insertHelper(root.right, node);
+            
+        }
+
+        return  root;
+
+    }
+    public void display(){
+
+    }
+    private void displayHelper(Node root){
+
+    }
+    public boolean search(int data) {
+        return false;
+    }
+    private boolean searchHelper (Node root, int data) {
+        return false;
+    }
+    public void remove(int data) {
+
+    }
+    private Node removeHelper(Node root, int data) {
+        return null;
+    }
+    private int sucessor(Node root) {
+        return 0;
+    }
+    private int predecessor(Node root) {
+        return 0;
+    }
+
+}
